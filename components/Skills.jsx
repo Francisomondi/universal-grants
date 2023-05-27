e40f9b0ffc4c86887dcb362cb341474f11b1cdb0
francis omondi <francisomondi17@gmail.com>
2023-05-27 13:24:56 +0300

    remoted lagging images

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -16,25 +16,25 @@ const Skills = () => {
   const skills = [
     {
       id: 1,
-      src: Html,
+      //src: Html,
       title: "Html",
       description: "My name is Susan Mutuku from Machakos. I did a tailoring course in 2009. I have been struggling to raise my kids since I have never gotten a reliable employment. A friend referred me to Universal Grants Ltd and I applied for Kes 100,000. My grants was approved and I received Kes 101,000. I bought my sewing machines, materials and rented a place for my business. My business is growing and I have employed 2 people currently I am very grateful to Universal Grants Ltd"
     },
     {
       id: 2,
-      src: css,
+      //src: css,
       title: "Css",
       description: "My name is Betty Shiku from Murang'a. I have been working as a house help in Nairobi since 2019. I came across an advert about Universal Grants for Business and tried my luck. I was awarded Ksh 150,000 and I used it to start a Mtumba business that my friend introduced me to. I am very thankful to Universal Grants Ltd. My business is doing great "
     },
     {
       id: 3,
-      src: javascript,
+      //src: javascript,
       title: "Javascript",
       description: " Am Karimi from Meru currently working in Kahawa West Nairobi. I am a salonist where I have been working for different people since 2005. I applied for Universal Grants and I was given Kes 200,000 to start my salon. My business is thriving very well and much thanks to Universal Grants Ltd"
     },
     {
       id: 4,
-      src: react,
+      //src: react,
       title: "React",
       description: "I am Grace from Kiambu doing Grocery. I am grateful for Universal Grants Ltd for giving me Capital worth Kes 75,000 which has helped me to grow this business to this level. Again!!!I thank Universal Grants Ltd."
     },
